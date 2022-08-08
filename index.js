ca27be2220dc58e3bcf9e2589b086e738ec42f93
Eduard Dzyurban <eduarddzyurban@gmail.com>
2022-08-08 22:11:27 +0300

    add home page

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
 import http from "http";
+import { sendHomePage } from "./senders/send-home-page.js";
+
+
+const getExecuterByUrl = {
+    "/": sendHomePage
+}
+
+const router = (req, res) => {
+    try {
+        const url = req.url;
+        const executer = getExecuterByUrl[url];
+
+        if (!executer) {
+            res.statusCode = 400;
+            return res.end();
+        }
+
+        return executer(req, res);
+    } catch (err) {
+        console.error(err);
+        res.statusCode = 500;
+        res.statusMessage = "Internal server error occured";
+        res.end();
+    }
+}
 
 const PORT = 8000;
-const server = http.createServer((req, res) => {
-    res.end("Server response")
-});
+const server = http.createServer(router);
 
 server.listen(PORT, () => console.log(`Server succesfully started at http://localhost:${PORT}`));
---
+++ senders/send-home-page.js
@@ -0,0 +1,11 @@
+import { fileURLToPath } from "url"
+import { join } from "path"
+import { createReadStream } from "fs";
+import { pipeline } from "stream";
+
+export const sendHomePage = (req, res) => {
+    const __dirname = fileURLToPath(new URL(".", import.meta.url));
+    const readStream = createReadStream(join(__dirname, "../public/views/index.html"));
+    res.setHeader("Content-Type", "text/html");
+    pipeline(readStream, res, (err) => err && console.log(err));
+};
